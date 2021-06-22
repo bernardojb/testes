f6c2430efc002a55d401f29651a4bf93e6114bb9
Bernardo Braga <bernardojbraga@gmail.com>
2021-06-21 21:39:44 -0300

    update api

--- pages/index.js
+++ pages/index.js
@@ -1,48 +1,61 @@
 import Head from 'next/head'
+import Image from 'next/image';
 import Header from '../components/header/header'
-// import Loading from '../components/loading/loading'
 import styles from './index.module.scss'
+import fetch from 'node-fetch';
+// import Loading from '../components/loading/loading'
 
 const Index = props => (
-    <div>
-      <Head>
-        <title>hokup I Transformação Digital</title>
-      </Head>
-      <section>
-        <Header />
-        <div className="home-container">
-          <div className="home-1">
-            <h1>
-              Home
-            </h1>
-          </div>
-          <div className="home-2">
-            <div className="cases_container">
-              <div className={styles.case}>
-                <div className={styles.case_content}>
-                  <div className={styles.pill_container}>
-                    <div className={styles.pill}>APP</div>
-                    <div className={styles.pill}>UI & UX</div>
-                  </div>
-                  <h1>Orthohack</h1>
-                  <p>Desenvolvimento de um app para iOS e Android, como ferramenta complementar do curso online de especialização em ortopedia.</p>
-                  <a>Link</a>
-                </div>  
-              </div>
-            </div>
-          </div>
-
+  <>
+    <Head>
+      <title>hokup I Transformação Digital</title>
+    </Head>
+    <section>
+      <Header />
+      <div className="home-container">
+        <div className="home-1">
+          <h1>
+            Home
+          </h1>
         </div>
-      </section>
-    </div>
-  );
+        <div className="home-2">
+          <div className="cases_container">
+            {props.products.map(product => (
+              <Link
+              key={product.id}
+              href='/products/[id]'
+              as={`/products/${product.id}`}>
+                <div className={styles.case}>
+                  <div className={styles.case_content}>
+                    <h1>{product.name}</h1>
+                    <p>{product.details}</p>
+                    <p>{product.price}</p>
+                    <a>Link</a>
+                  </div>
+                  <Image
+                  key={product.image}
+                  href={product.image}
+                  width={150}
+                  height={150}
+                  />
+                </div>
+              </Link>
 
-  Index.getInitialProps = async function() {
-    const res = await fetch(
+            ))}
+
+          </div>
+        </div>
+      </div>
+    </section>
+  </>
+);
+
+Index.getInitialProps = async function () {
+  const res = await fetch(
     "https://my-json-server.typicode.com/bernardojb/testes/cases"
-    );
-    const data = await res.json();
-    return{
-      cases: data
-    };
-  }
+  );
+  const data = await res.json();
+  return {
+    cases: data
+  };
+}
