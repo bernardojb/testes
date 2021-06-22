a3103b14bd347304711a48e40b8e635b1de904da
Bernardo Braga <bernardojbraga@gmail.com>
2021-06-21 21:28:37 -0300

    teste 2

--- pages/index.js
+++ pages/index.js
@@ -1,21 +1,48 @@
 import Head from 'next/head'
 import Header from '../components/header/header'
-import Loading from '../components/loading/loading'
+// import Loading from '../components/loading/loading'
+import styles from './index.module.scss'
 
-export default function Home() {
-  return (
-    <Loading>
+const Index = props => (
+    <div>
       <Head>
         <title>hokup I Transformação Digital</title>
       </Head>
       <section>
         <Header />
-        <div>
-          <h1>
-            Home
-          </h1>
+        <div className="home-container">
+          <div className="home-1">
+            <h1>
+              Home
+            </h1>
+          </div>
+          <div className="home-2">
+            <div className="cases_container">
+              <div className={styles.case}>
+                <div className={styles.case_content}>
+                  <div className={styles.pill_container}>
+                    <div className={styles.pill}>APP</div>
+                    <div className={styles.pill}>UI & UX</div>
+                  </div>
+                  <h1>Orthohack</h1>
+                  <p>Desenvolvimento de um app para iOS e Android, como ferramenta complementar do curso online de especialização em ortopedia.</p>
+                  <a>Link</a>
+                </div>  
+              </div>
+            </div>
+          </div>
+
         </div>
       </section>
-    </Loading>
-  )
-}
+    </div>
+  );
+
+  Index.getInitialProps = async function() {
+    const res = await fetch(
+    "https://my-json-server.typicode.com/bernardojb/testes/cases"
+    );
+    const data = await res.json();
+    return{
+      cases: data
+    };
+  }
